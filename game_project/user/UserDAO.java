28e0ff1d8910078af679403cbca5a2e482303d31
hyogyeong <cdkrcd8@gmail.com>
2025-08-19 00:03:36 +0900

    로그인 정보 추가

--- game_project/user/UserDAO.java
+++ game_project/user/UserDAO.java
@@ -1,47 +1,134 @@
-package game_project.user;
+package user;
 
-import java.util.HashMap;
-
-import game_project.interfaces.user.UserDAOInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import interfaces.user.UserDAOInterface;
+import util.DBConnect;
 
 // ## DAO에서는 DB관련 작업만 수행 ##
 public class UserDAO implements UserDAOInterface{
-  public static final HashMap<Integer, UserDTO> userMap = new HashMap<>();
-  private static int pk;
-  static {
-    userMap.put(1, new UserDTO("user1", "hong", "hong123"));
-    userMap.put(2, new UserDTO("user2", "kim", "kim123"));
-    userMap.put(3, new UserDTO("user3", "lee", "lee123"));
-    pk = 4;
+  Connection connection = null;
+  PreparedStatement pstmt = null;
+  ResultSet rs = null;
+
+  @Override
+  public boolean addUser(UserDTO u) throws Exception {
+    try {
+      connection = DBConnect.getConnection();
+      String sql = "insert into account (userid, name, userpw) values (?,?,?)";
+      pstmt = connection.prepareStatement(sql);
+      pstmt.setString(1, u.getId());
+      pstmt.setString(2, u.getName());
+      pstmt.setString(3, u.getPw());
+      int rslt = pstmt.executeUpdate();
+      if (rslt >= 0){
+        return true;
+      }
+    } catch (Exception e){
+      throw e;
+    }
+    return false;
   }
 
-  public static int getPk() {
-    return pk;
+  @Override
+  public String getUserPK(String userId) throws Exception{
+    try {
+      connection = DBConnect.getConnection();
+      String sql = "select * from account where userid=?";
+      pstmt = connection.prepareStatement(sql);
+      pstmt.setString(1, userId);
+      rs = pstmt.executeQuery();
+      if (rs.next()){
+        return rs.getString("id");
+      }
+    } catch (Exception e){
+      throw e;
+    }
+    return "";
   }
 
-  public static void setPk(int pk) {
-    UserDAO.pk = pk;
+  @Override
+  public boolean deleteUser(String userPK) throws Exception{
+     try {
+      connection = DBConnect.getConnection();
+      String sql = "delete from account where id=?";
+      pstmt = connection.prepareStatement(sql);
+      pstmt.setString(1, userPK);
+      int rslt = pstmt.executeUpdate();
+      if (rslt > 0){
+        return true;
+      }
+     } catch (Exception e){
+      throw e;
+    } finally {
+      DBConnect.close(connection, pstmt, rs);
+    }
+    return false;
+  }
+
+  @Override
+  public boolean checkIfUserExists(UserDTO dto) throws Exception{
+    try {
+      connection = DBConnect.getConnection();
+      String sql = "select * from account where userid=? and userpw=?";
+      pstmt = connection.prepareStatement(sql);
+      pstmt.setString(1, dto.getId());
+      pstmt.setString(2, dto.getPw());
+      rs = pstmt.executeQuery();
+      if (rs.next()){
+        return true;
+      }
+    } catch (Exception e){
+      throw e;
+    } finally {
+      DBConnect.close(connection, pstmt, rs);
+    }
+    return false;
+  }
+
+  @Override
+  public boolean login(String userId, int ifLogin) throws Exception{
+    try {
+      connection = DBConnect.getConnection();
+      String sql = "update account set iflogin=? where userid=?";
+      pstmt = connection.prepareStatement(sql);
+      if (ifLogin == 0){
+        pstmt.setInt(1, 1);
+      } else {
+        pstmt.setInt(1, 0);
+      }
+      pstmt.setString(2, userId);
+      int rslt = pstmt.executeUpdate();
+      if (rslt > 0){
+        return true;
+      }
+    } catch (Exception e){
+      throw e;
+    }
+    return false;
   }
   
   @Override
-  public void addUser(UserDTO newUser) {
-    userMap.put(pk, newUser);
-    setPk(pk+1);
+  public boolean logOut(String userPK, int ifLogin) throws Exception{
+    try {
+      connection = DBConnect.getConnection();
+      String sql = "update account set iflogin=? where id=?";
+      pstmt = connection.prepareStatement(sql);
+      if (ifLogin == 0){
+        pstmt.setInt(1, 1);
+      } else {
+        pstmt.setInt(1, 0);
+      }
+      pstmt.setString(2, userPK);
+      int rslt = pstmt.executeUpdate();
+      if (rslt > 0){
+        return true;
+      }
+    } catch (Exception e){
+      throw e;
+    }
+    return false;
   }
-
-  @Override
-  public void deleteUserByPK(int pk) {
-    userMap.remove(pk);
-  }
-
-  @Override
-  public UserDTO getUserByPK(int pk) {
-    return userMap.get(pk);
-  }
-
-  @Override
-  public void updateUser(int pk, UserDTO newUser) {
-    userMap.remove(pk);
-    userMap.put(pk, newUser);
-  }
+  
 }
--- game_project/user/UserDTO.java
+++ game_project/user/UserDTO.java
@@ -1,9 +1,13 @@
-package game_project.user;
+package user;
 
 public class UserDTO {
+  private String pk;
   private String id;
   private String name;
   private String pw;
+  private int iflogin;
+
+  public UserDTO(){}
 
   public UserDTO(String id, String name, String pw) {
     this.id = id;
@@ -11,6 +15,20 @@ public class UserDTO {
     this.pw = pw;
   }
 
+  public UserDTO(String pk, String id, String name, String pw, int iflogin) {
+    this.pk = pk;
+    this.id = id;
+    this.name = name;
+    this.pw = pw;
+    this.iflogin = iflogin;
+  }
+
+  public String getPk() {
+    return pk;
+  }
+  public void setPk(String pk) {
+    this.pk = pk;
+  }
   public String getId() {
     return id;
   }
@@ -29,10 +47,15 @@ public class UserDTO {
   public void setPw(String pw) {
     this.pw = pw;
   }
+  public int getIflogin() {
+    return iflogin;
+  }
+  public void setIflogin(int iflogin) {
+    this.iflogin = iflogin;
+  }
 
   @Override
   public String toString(){
     return "사용자 정보:\n이름 : "+this.name+", id : "+this.id;
   }
-  
 }
