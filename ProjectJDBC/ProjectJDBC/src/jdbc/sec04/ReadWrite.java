9705541eae7874f1a07fe9df557aa05df921ca3e
hyogyeong <cdkrcd8@gmail.com>
2025-08-22 09:20:25 +0900

    scanner로 읽기, print문 구성

--- ProjectJDBC/ProjectJDBC/src/jdbc/sec04/ReadWrite.java
+++ ProjectJDBC/ProjectJDBC/src/jdbc/sec04/ReadWrite.java
@@ -1,6 +1,5 @@
 package jdbc.sec04;
 
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,7 +7,7 @@ import java.util.Scanner;
 
 public class ReadWrite {
     public static BookDTO readBookInfo(Scanner sc){
-        System.out.println("추가할 도서 정보를 입력하세요");
+        System.out.println("도서 정보를 입력하세요");
         BookDTO dto = null;
         PublisherDAO pubDAO = new PublisherDAO();
         try {
@@ -18,8 +17,14 @@ public class ReadWrite {
             System.out.print("도서명 입력 : ");
             String bookName = sc.nextLine();
 
+            System.out.print("저자명 입력 : ");
+            String bookAuthor = sc.nextLine();
+
             System.out.print("도서 가격 입력 : ");
             int bookPrice = Integer.parseInt(sc.nextLine());
+
+            System.out.print("도서 재고 수량 입력 : ");
+            int bookStock = Integer.parseInt(sc.nextLine());
 
             System.out.print("도서 출간일 입력 : ");
             String strDate = sc.nextLine();
@@ -30,9 +35,9 @@ public class ReadWrite {
             System.out.print("출판사 번호 입력 : ");
             String pubNo = sc.nextLine();
 
-            dto = new BookDTO(bookNo, bookName, bookPrice, bookDate, pubNo);
+            dto = new BookDTO(bookNo, bookName, bookAuthor, bookPrice, bookDate, bookStock, pubNo);
             
-        } catch (SQLException e){
+        } catch (Exception e){
             e.printStackTrace();
         }
         return dto;
@@ -40,25 +45,61 @@ public class ReadWrite {
     
     public static void writeAllBooks(ArrayList<BookDTO> bookList){
         System.out.println("************전체 도서 정보************");
+        // 헤더
+        System.out.printf("%-8s %-15s %-15s %8s %-10s %8s %-8s%n",
+            "BOOKNO", "BOOKNAME", "BOOKAUTHOR", "BOOKPRICE", "BOOKDATE", "BOOKSTOCK", "PUBNO");
+
         for (BookDTO book : bookList){
             String bookNo = book.getBookNo();
             String bookName = book.getBookName();
             int bookPrice = book.getBookPrice();
             Date bookDate = book.getBookDate();
             String pubNo = book.getPubNo(); 
+            String bookAuthor = book.getBookAuthor();
+            int bookStock = book.getBookStock();
+            // 데이터
+            System.out.printf("%-8s %-15s %-15s %8d %-10s %8d %-8s%n",
+                bookNo, bookName, bookAuthor, bookPrice, bookDate.toString(), bookStock, pubNo);
         }
     }
     
-    public static BookDTO searchBookById(Scanner sc){
-        
-    }
+    public static void writeBookDetail(BookDTO book){
+        System.out.println("************검색한 도서 정보************");
+        // 헤더
+        System.out.printf("%-8s %-15s %-15s %8s %-10s %8s %-8s%n %-8s",
+            "BOOKNO", "BOOKNAME", "BOOKAUTHOR", "BOOKPRICE", "BOOKDATE", "BOOKSTOCK", "PUBNO", "PUBNAME");
 
-    public static ArrayList<BookDTO> searchBookByName(String bookName){
-        
+        String bookNo = book.getBookNo();
+        String bookName = book.getBookName();
+        int bookPrice = book.getBookPrice();
+        Date bookDate = book.getBookDate();
+        String pubNo = book.getPubNo(); 
+        String bookAuthor = book.getBookAuthor();
+        int bookStock = book.getBookStock();
+        String publisher = book.getPublisher();
+        // 데이터
+        System.out.printf("%-8s %-15s %-15s %8d %-10s %8d %-8s%n %-8s",
+            bookNo, bookName, bookAuthor, bookPrice, bookDate.toString(), bookStock, pubNo, publisher);
     }
-
-    public static BookDTO searchBookByBookNo(String bookNo){
-        
+    
+    public static void writeAllBookDetail(ArrayList<BookDTO> bookList){
+        System.out.println("************검색한 도서 정보************");
+        // 헤더
+        System.out.printf("%-8s %-15s %-15s %8s %-10s %8s %-8s%n %-8s",
+            "BOOKNO", "BOOKNAME", "BOOKAUTHOR", "BOOKPRICE", "BOOKDATE", "BOOKSTOCK", "PUBNO", "PUBNAME");
+        for (BookDTO book : bookList){
+            String bookNo = book.getBookNo();
+            String bookName = book.getBookName();
+            int bookPrice = book.getBookPrice();
+            Date bookDate = book.getBookDate();
+            String pubNo = book.getPubNo(); 
+            String bookAuthor = book.getBookAuthor();
+            int bookStock = book.getBookStock();
+            String publisher = book.getPublisher();
+            // 데이터
+            System.out.printf("%-8s %-15s %-15s %8d %-10s %8d %-8s%n %-8s",
+                bookNo, bookName, bookAuthor, bookPrice, bookDate.toString(), bookStock, pubNo, publisher);
+        }
     }
     
     private static void writeAllPublisher(ArrayList<PublisherDTO> pubList){
@@ -69,6 +110,5 @@ public class ReadWrite {
         }
         System.out.println("-------------------------------");
     }
-    System.out.format("%-3s\t %-20s\t %-10d %10s %10s \t%3d\n",
-                                    PRDNO,PRDNAME,PRDPRICE,PRDMAKER,PRDCOLOR,CTGNO);
+    
 }
