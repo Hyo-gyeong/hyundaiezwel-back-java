bf5d7210477f9a0c9e603a3dc719aae8bce2c654
hyogyeong <cdkrcd8@gmail.com>
2025-08-22 09:18:32 +0900

    dto 구성

--- ProjectJDBC/ProjectJDBC/src/jdbc/sec04/BookDTO.java
+++ ProjectJDBC/ProjectJDBC/src/jdbc/sec04/BookDTO.java
@@ -5,18 +5,49 @@ import java.util.Date;
 public class BookDTO {
     private String bookNo;
     private String bookName;
+    private String bookAuthor;
     private int bookPrice;
     private Date bookDate;
+    private int bookStock;
     private String pubNo;
+    private String publisher;
     
-    public BookDTO(String bookNo, String bookName, int bookPrice, Date bookDate, String pubNo) {
+
+    public BookDTO(String bookNo, String bookName, String bookAuthor, int bookPrice, Date bookDate, int bookStock,
+            String pubNo, String publisher) {
         this.bookNo = bookNo;
         this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
         this.bookPrice = bookPrice;
         this.bookDate = bookDate;
+        this.bookStock = bookStock;
+        this.pubNo = pubNo;
+        this.publisher = publisher;
+    }
+
+    public BookDTO(String bookNo, String bookName, String bookAuthor, int bookPrice, Date bookDate, int bookStock,
+            String pubNo) {
+        this.bookNo = bookNo;
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.bookDate = bookDate;
+        this.bookStock = bookStock;
         this.pubNo = pubNo;
     }
     
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+    public int getBookStock() {
+        return bookStock;
+    }
+    public void setBookStock(int bookStock) {
+        this.bookStock = bookStock;
+    }
     public String getBookNo() {
         return bookNo;
     }
@@ -46,6 +77,14 @@ public class BookDTO {
     }
     public void setPubNo(String pubNo) {
         this.pubNo = pubNo;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
     
 }
