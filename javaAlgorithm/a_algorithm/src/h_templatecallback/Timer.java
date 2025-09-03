e9818866e1b50e219406d8da01bf133684bf657f
hyogyeong <cdkrcd8@gmail.com>
2025-09-04 00:10:39 +0900

    timer 생성

---
+++ javaAlgorithm/a_algorithm/src/h_templatecallback/Timer.java
@@ -0,0 +1,7 @@
+package h_templatecallback;
+
+@FunctionalInterface // 람다로 사용하기 위한 인터페이스임을 알 수 있음
+public interface Timer {
+    void start();
+    // void end(); // 어노테이션으로 인해 오버라이드할 메서드 개수 제한 가능하게 해줌
+}
