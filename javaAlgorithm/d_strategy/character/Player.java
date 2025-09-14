5f1523f81a38741d3e84ddbc27baf5fd32d44124
hyogyeong <cdkrcd8@gmail.com>
2025-09-14 22:25:55 +0900

    모험 캐릭터 구성

---
+++ javaAlgorithm/d_strategy/character/Player.java
@@ -0,0 +1,67 @@
+package d_strategy.character;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Random;
+
+import d_strategy.item.Equipment;
+import d_strategy.item.Equipments;
+import d_strategy.item.Slot;
+import d_strategy.item.code.EquipmentData;
+
+public class Player extends Character{
+	
+	private Random random = new Random();
+	// private Map<Slot, Equipment> equipments = new HashMap<Slot, Equipment>();
+    private Equipments equipments = new Equipments();
+
+	public Player(String name, int hp, int atk, int def) {
+		super(name, hp, atk, def);
+	}
+
+	@Override
+	public void attack(Character target) {
+		int damage = random.nextInt(atk, atk * 3);
+		
+		System.out.println("[" + name + "] 공격!");
+		for(Equipment e : equipments.findAll()) {
+			damage = e.calAttackWeight(damage);
+		}
+		
+        Optional<Equipment> optional = equipments.findBySlot(Slot.WEAPON);
+        // optional.isPresent();
+        // optional.isEmpty();
+        // optional.orElse(EquipmentData.BASIC_WEAPON.create());
+        // optional.orElseGet(() -> EquipmentData.BASIC_WEAPON.create());
+        // optional.orElseThrow(() -> new NoSuchElementException()); (* optional.get();)
+		// if(equipments.findBySlot(Slot.WEAPON) != null) {
+		// 	System.out.println(equipments.get(Slot.WEAPON).getEffect()); 
+		// }
+        
+        if (optional.isPresent()){
+            System.out.println(optional.get().getEffect());
+        }
+        target.takeDamage(damage);
+	}
+
+	@Override
+	public void takeDamage(int damage) {
+		// 피해량 연산
+		for(Equipment e : equipments.findAll()) {
+			damage = e.calDefenceWeight(damage);
+		}
+		System.out.println("[ Damage ] :" + damage);
+		
+		this.currentHp -= damage;
+		this.currentHp = Math.max(currentHp, 0);
+	}
+
+	public void equip(Equipment equipment) {
+		equipments.equip(equipment);
+	}
+	
+	
+
+}
