97a6e8caf7b0c31ad2acef6b43c57e653d96caa2
hyogyeong <cdkrcd8@gmail.com>
2025-09-24 16:49:59 +0900

    event domain 구성

---
+++ javaAlgorithm/tcp/src/com/mc/trpg/domain/event/AbstractEvent.java
@@ -0,0 +1,23 @@
+package com.mc.trpg.domain.event;
+
+import com.mc.trpg.domain.character.Player;
+import com.mc.trpg.domain.event.code.EventStatus;
+
+public abstract class AbstractEvent implements Event{
+
+	protected EventStatus status;
+	protected Player player;
+	
+	public AbstractEvent(EventStatus status, Player player) {
+		super();
+		this.status = status;
+		this.player = player;
+	}
+
+	@Override
+	public EventStatus status() {
+		return status;
+	}
+	
+	
+}
---
+++ javaAlgorithm/tcp/src/com/mc/trpg/domain/event/Event.java
@@ -0,0 +1,11 @@
+package com.mc.trpg.domain.event;
+
+import com.mc.trpg.domain.event.code.EventStatus;
+
+public interface Event {
+
+	void proceed();
+	EventStatus status();
+	String desc();
+	
+}
